b64a8185c62addec39ed5cb9b3e360ff6eb20650
ginnygu <ginnygu@gmail.com>
2023-05-15 15:28:35 -0400

    initial commit

---
+++ functions.js
@@ -0,0 +1,41 @@
+const returnEmptyString = function () {
+	return "";
+};
+
+const returnZeroNumber = function () {};
+
+const returnEmptyArray = function () {};
+
+const returnEmptyObject = function () {};
+
+const returnAbcString = function (value) {};
+
+const returnSumOfTwo = function (arg1, arg2) {};
+
+const subtraction = function (arg1, arg2) {};
+
+const multiplication = function (arg1, arg2) {};
+
+const division = function (arg1, arg2) {};
+
+const returnArray = function (arg1) {};
+
+const returnFirstIndex = function (arg1) {};
+
+const returnSecondIndex = function (arg1) {};
+
+const returnArrayLength = function (arg1) {};
+
+const arraySum = function (arg1) {};
+
+const arraySubtraction = function (arg1) {};
+
+const multiplicationArray = function (arg1) {};
+
+const divisionArray = function (arg1) {};
+
+const oddArray = function (arg1) {};
+
+const evenArray = function (arg1) {};
+
+const returnFruits = function (arg1) {};
